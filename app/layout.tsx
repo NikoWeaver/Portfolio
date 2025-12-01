548f79b57feb3a6e0234df67c53a74d9fce3c670
nikoweaver <nikoweaver@gmail.com>
2025-11-30 21:49:42 -0500

    e

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import Link from "next/link"
-import "@/styles/globals.css"
+import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ThemeToggle } from "@/components/ui/theme-toggle"
@@ -54,5 +54,3 @@ export default function RootLayout({
     </html>
   )
 }
-
-import "./globals.css"
